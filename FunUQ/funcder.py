Update to Potential class Example updates for nH Bug fixes for functional derivatives

FunUQ/funcder.py
@@ -6,12 +6,12 @@ import os, numpy as np
 from glob import glob
 from random import random
 from operator import methodcaller
+from copy import deepcopy
 from matplotlib import pyplot as plt
 
 from .qoi import fluctuation
-from .potential import create_table
 from .parsetools import read_thermo
-from .utils import copy_files, read_template, replace_template, submit_lammps
+from .utils import copy_files, write_file, read_template, replace_template, submit_lammps
 
 class FuncDer(object): 
     '''
@@ -28,16 +28,8 @@ class FuncDer(object):
         self.pot = potential 
         self.ensemble = ensemble
 
-        '''
-        if self.ensemble == 'nvt' and 'Volume' in self.QoI.Q_names:
-            self.QoI.Q_names.remove('Volume')
-            print("Cannot predict volume with constant volume")
-        if self.ensemble == 'npt' and 'Press' in self.QoI.Q_names:
-            self.QoI.Q_names.remove('Press')
-            print("Cannot predict pressure with constant pressure")
-        '''
         # Debugging
-        self.showA = True
+        self.showA = False
 
         # Perturbations
         self.rdfbins = 2000
@@ -50,10 +42,6 @@ class FuncDer(object):
         #self.alist = [-0.000375, -0.00075, 0.00075, 0.000375]
         self.clist = [0.1]
 
-        self.newshape = (self.QoI.Ntimes*self.QoI.Ncopies, len(self.rlist), len(self.alist),
-                         len(self.clist), len(self.QoI.Q_names))
-        self.Qprime = np.zeros([self.QoI.Ntimes, self.QoI.Ncopies, len(self.rlist), len(self.alist),
-                         len(self.clist), len(self.QoI.Q_names)])
         self.method = 'none'
         self.FDnames = []
 
@@ -64,6 +52,11 @@ class FuncDer(object):
                     setattr(self, key, val)
                 except:
                     raise KeyError("{} is not a valid input parameter.".format(key))
+
+        self.newshape = (self.QoI.Ntimes*self.QoI.Ncopies, len(self.rlist), len(self.alist),
+                         len(self.clist), len(self.QoI.Q_names))
+        self.Qprime = np.zeros([self.QoI.Ntimes, self.QoI.Ncopies, len(self.rlist), len(self.alist),
+                         len(self.clist), len(self.QoI.Q_names)])
 
 
     def get_names(self):
@@ -141,7 +134,7 @@ class FuncDer(object):
                     self.funcder[r0c, qc] = m[0]
 
 
-                    if self.showA  and 'HeatCapacity' in q:
+                    if self.showA and 'HeatCapacity' in q:
                         print(m[0], r0, np.max(self.Qperturbed[r0c, :, c0c, qc]) - np.min(self.Qperturbed[r0c, :, c0c, qc])) #, self.alist)
                         plt.xlim([np.min(self.alist), np.max(self.alist)])
                         plt.ylim([np.min(self.Qperturbed[r0c, :, c0c, qc]), np.max(self.Qperturbed[r0c, :, c0c, qc])])
@@ -152,7 +145,7 @@ class FuncDer(object):
 
             self.funcder[:,qc] = self.funcder[:,qc]*self.QoI.conversions[qc]
 
-        print("Calculated functional derivative")
+        print("Calculated functional derivatives\n")
 
 
 
@@ -195,9 +188,7 @@ class FuncDer_bruteforce(FuncDer):
 
                     out = '{}_{}_{}_{}'.format(self.pot.potname, r0, a0, c0)
                     potfile = '{}.table'.format(out)
-                    R, PE, style, coeff = create_table(self.pot.potname, self.pot.parampath,
-                                                       #self.pot, self.parampath,
-                                                       r0=r0, a0=a0, c0=c0, 
+                    R, PE, style, coeff = self.pot.create_table(r0=r0, a0=a0, c0=c0, 
                                                        outname=potfile, savedir=adir)
                     
                     # This is to fix data path IF ../ is there (as it should be for multiple copies
@@ -219,14 +210,14 @@ class FuncDer_bruteforce(FuncDer):
 
     # Replace with call to extrct_lmp
     def extract_lammps(self):
-        Qperturbed = np.zeros([self.QoI.Ntimes, self.QoI.Ncopies, len(self.rlist),                                                                
+        Qperturbed = np.zeros([self.QoI.Ntimes, self.QoI.Ncopies, len(self.rlist),
                                len(self.alist), len(self.clist), len(self.QoI.Q_names)])
         first = True
         for c0c, c0 in enumerate(self.clist):
             for r0c, r0 in enumerate(self.rlist):
                 for a0c, a0 in enumerate(self.alist):
                     for copy0, copy in enumerate(range(self.QoI.copy_start, self.QoI.copy_start + self.QoI.Ncopies)):
-                        #rdir = 'r{:.4f}'.format(r0)[:-2]
+
                         logfile = glob(os.path.join(self.rundir,
                                                     'c{}'.format(c0), 'r{:.2f}*'.format(r0), 'a{}'.format(a0),
                                                     self.QoI.copy_folder+str(copy), self.QoI.logfile))[0]
@@ -260,7 +251,7 @@ class FuncDer_bruteforce(FuncDer):
                             elif qn == 'ThermalExpansion':
                                 Qperturbed[:, copy0, r0c, a0c, c0c, qc] = thermo[:,self.QoI.Q_cols[self.QoI.Vcol]]**2
 
-                print("Extracted thermo data for position {}".format(r0))
+                print("Extracted thermodynamic data for position {}".format(r0))
 
         self.Qperturbed = np.mean(Qperturbed, axis=(0,1))
 
@@ -270,27 +261,7 @@ class FuncDer_bruteforce(FuncDer):
                 #self.Qperturbed[ = np.mean(Qperturbed, axis=(0,1))
 
             if not self.QoI.Q_thermo[qc]:
-                print(self.QoI.Q_names[qc])
                 self.funcder_fluct(q, qc)
-            '''
-            if q == 'HeatCapacityVol':
-                #Q = self.Qperturbed[:,:,0,0,0,self.PEcol]
-                #Q2 = self.Q[:,:,0,0,0,qc] # already squared                                                                                   
-                self.Qperturbed[:,:,:,qc] = fluctuation(self.QoI.beta/self.QoI.T, self.Qperturbed[:,:,:,qc],
-                                                        self.Qperturbed[:,:,:,self.QoI.PEcol])
-                #self.QoI.beta/(self.QoI.T)*(self.Qperturbed[:,:,:,qc] - self.Qperturbed[:,:,:,self.QoI.PEcol]**2)
-            elif q == 'HeatCapacityPress':
-                self.Qperturbed[:,:,:,qc] = fluctuation(self.QoI.beta/self.QoI.T, self.Qperturbed[:,:,:,qc],
-                                                        self.Qperturbed[:,:,:,self.QoI.PEcol]+
-                                                        self.Qperturbed[:,:,:,self.QoI.Pcol]*self.Qperturbed[:,:,:,self.QoI.Vcol])
-            elif q == 'Compressibility':
-                self.Qperturbed[:,:,:,qc] = fluctuation(self.beta/self.V, self.Qperturbed[:,:,:,qc], 
-                                                        self.Qperturbed[:,:,:,self.QoI.Vcol])
-            elif q == 'ThermalExpansion':
-                self.Qperturbed[:,:,:,qc] = fluctuation(self.beta/self.V, self.Qperturbed[:,:,:,qc],
-                                                        self.Qperturbed[:,:,:,self.QoI.PEcol]+
-                                                        self.Qperturbed[:,:,:,self.QoI.Pcol]*self.Qperturbed[:,:,:,self.QoI.Vcol])
-            '''
             
             
     def prepare_FD(self):
@@ -312,13 +283,10 @@ class FuncDer_perturbative(FuncDer):
 
 
     def exp_weighting(self):
-        #newshape = (self.Ntimes*self.Ncopies, len(self.rlist), len(self.alist), len(self.clist))
-
         # Get Q for weighting 
         Hprime = self.Qprime[:,:,:,:,:,self.QoI.PEcol]
-        #tmp = Hprime.reshape(newshape)
-        #Hprime_avg = np.mean(tmp, axis=0)
         Hprime_avg = np.mean(Hprime, axis=(0,1))
+        #Qprime_avg = np.mean(self.Qprime, axis=(0,1))
         if self.ensemble == 'npt':
             Vprime = self.Qprime[:,:,:,:,:,self.QoI.Vcol]
             Vprime_avg = np.mean(Vprime, axis=(0,1))
@@ -328,6 +296,8 @@ class FuncDer_perturbative(FuncDer):
         # Add unperturbed QoI 
         Qprime = self.Qprime + self.QoI.Q # eV**2 + eV**2
 
+        #print(np.shape(Hprime_avg))
+        #print(Hprime_avg[0,0,0], self.QoI.beta*(Hprime-Hprime_avg)[0,0,0])
         # Average the exponential
         if self.ensemble == 'nvt':
             expweights = np.exp(-self.QoI.beta*(Hprime-Hprime_avg))
@@ -335,30 +305,24 @@ class FuncDer_perturbative(FuncDer):
             expweights = np.exp(-self.QoI.beta*((Hprime-Hprime_avg) + 
                                                 (Vprime-Vprime_avg)*(Pprime-Pprime_avg)))
 
-        #tmp = expweights.reshape(newshape)
-        #denominator = np.mean(tmp, axis=0)
         denominator = np.mean(expweights, axis=(0,1))
 
         # Average the QoI*exponential product
         self.Qperturbed = np.zeros([len(self.rlist), len(self.alist), len(self.clist), len(self.QoI.Q_names)])
         for qc, (q,thermoq) in enumerate(zip(self.QoI.Q_names, self.QoI.Q_thermo)):
-            numerator = expweights*Qprime[:,:,:,:,:,qc]
+            #Qprime = self.Qprime[:,:,:,:,:,qc]
+            #Qprime_avg = np.mean(Qprime, axis=(0,1,2,3,4))
 
-            #tmp = numerator.reshape(newshape)
-            #numerator = np.mean(tmp, axis=0)
+            #print(np.mean(self.Qprime[:,:,:,:,:,qc]), np.mean(self.QoI.Q[:,:,:,:,:,qc]))
+            numerator = expweights*(Qprime[:,:,:,:,:,qc])
+            #numerator = expweights*(Qprime-Qprime_avg)
+
             numerator = np.mean(numerator, axis=(0,1))
             self.Qperturbed[:,:,:,qc] = numerator/denominator
 
+        for qc, q in enumerate(self.QoI.Q_names):
             if not thermoq:
-                print(q, thermoq)
                 self.funcder_fluct(q, qc)
-
-            #if q == 'HeatCapacityVol':
-                #self.QoI.beta/(self.QoI.T)*(self.Qperturbed[:,:,:,self.QoI.PEcol]**2 - self.Qperturbed[:,:,:,qc])
-
-
-        #for qc, q in enumerate(self.QoI.Q_names):
-        #    self.Qperturbed[:,:,:,qc] = self.Qperturbed[:,:,:,qc]*self.QoI.conversions[qc]
 
 
 
@@ -374,16 +338,14 @@ class FuncDer_perturb_allatom(FuncDer_perturbative):
 
         self.reruntemplate = 'rerun.template'
         #self.rerunfile = 'in.rerun_gauss'
-        self.rerunpath = os.path.join(self.initdir, self.reruntemplate)
+        self.rerunpath = os.path.join(self.QoI.initdir, self.reruntemplate)
         self.reruntxt = read_template(self.rerunpath)
 
-        self.rerunpotdir = os.path.join(self.rundir, 'gauss')
+        self.rerunpotdir = os.path.join(self.QoI.rundir, 'gauss')
 
         self.rerunsubtemplate = 'submit_rerun.template'
-        self.rerunsubpath = os.path.join(self.initdir, self.rerunsubtemplate)
+        self.rerunsubpath = os.path.join(self.QoI.initdir, self.rerunsubtemplate)
         self.rerunsubtxt = read_template(self.rerunsubpath)
-
-        #super(FuncDer_perturb_allatom, self).__init__(*args, **kwargs)
 
 
     def rerun_gauss(self):
@@ -406,7 +368,7 @@ class FuncDer_perturb_allatom(FuncDer_perturbative):
                         inname = 'in.{}'.format(out)
                         if not copy0:
                             potname = '{}.table'.format(out)
-                            R, PE, style, coeff = create_table('gauss', '', r0=r0, a0=a0, c0=c0, 
+                            R, PE, style, coeff = self.pot.create_table('gauss', '', r0=r0, a0=a0, c0=c0, 
                                                                outname=potname,
                                                                savedir=self.rerunpotdir)
                         replace = {'LOGNAME': outname,
@@ -417,7 +379,7 @@ class FuncDer_perturb_allatom(FuncDer_perturbative):
 
             replace_sub = {'NAME': 'gauss'}
             subtxt = replace_template(self.rerunsubtxt, replace_sub)
-            submit_lammps(self.subfile, subtxt,
+            submit_lammps(self.QoI.subfile, subtxt,
                           'tmp', '', rerundir)
 
 
@@ -432,16 +394,14 @@ class FuncDer_perturb_allatom(FuncDer_perturbative):
                         logfile = os.path.join(self.QoI.rundir, self.QoI.copy_folder+str(copy),
                                                'rerun_gauss/', 'gauss_{}_{}_{}.log'.format(r0, a0, c0))
                         cols, thermo, Natoms = read_thermo(logfile)
+
                         for qc, q in enumerate(self.QoI.Q_cols):
-                            self.Qprime[:, copy0, r0c, a0c, c0c, qc] = thermo[:,q]
+                            if self.QoI.Q_thermo[qc]:
+                                self.Qprime[:, copy0, r0c, a0c, c0c, qc] = thermo[:,q]
 
-                print("Extracted thermo data for perturbation position {}".format(r0))
-
+                print("Extracted thermodynamic data for perturbation position {}".format(r0))
 
     def prepare_FD(self):
-        #self.extract_lammps(self.mainpot)
-        #print("Extracted thermo data")
-
         self.extract_lammps_gauss()
         print("Extracted perturbation data")
 
@@ -518,8 +478,8 @@ class FuncDer_perturb_coord(FuncDer_perturbative):
                             #gauss, gaussdiff = self.get_perturb(r0, a0, c0)
                             
                             for qc, q in enumerate(self.QoI.Q_names):
-
-                                if q == 'PotEng':
+                                
+                                if q == 'PotEng' or q == 'E_vdwl':
                                     #self.Qprime[tc,copy,r0c,a0c,c0c,qc] = np.sum(self.gauss[r0c,a0c,c0c, :]*
                                     #                                             self.coord[tc, copy, :])
                                     self.Qprime[tc,copy0,r0c,a0c,c0c,qc] = np.sum(self.gauss[r0c,a0c,c0c, :]*coord[tc,:])
@@ -529,7 +489,7 @@ class FuncDer_perturb_coord(FuncDer_perturbative):
                                     self.Qprime[tc,copy0,r0c,a0c,c0c,qc] = np.sum(self.gaussdiff[r0c,a0c,c0c, :]*coord[tc,:]*self.rdf_R[:-1])
 
         for qc, q in enumerate(self.QoI.Q_names):
-            if q == 'PotEng':
+            if q == 'PotEng' or q == 'E_vdwl':
                 #self.Qprime[:,:,:,:,:,qc] = np.sum(self.gauss*
                 #                                   self.coord, axis=-1)
                 self.Qprime[:,:,:,:,:,qc] = self.Qprime[:,:,:,:,:,qc]*self.QoI.Natoms/2.
@@ -538,7 +498,7 @@ class FuncDer_perturb_coord(FuncDer_perturbative):
                 #                                   self.coord*self.rdf_R, axis=-1)
                 self.Qprime[:,:,:,:,:,qc] = (self.Qprime[:,:,:,:,:,qc]/3./self.QoI.V)*160.2176*10000.*self.QoI.Natoms/2.
             elif q == 'HeatCapacityVol': # copying issue? 
-                self.Qprime[:,:,:,:,:,qc] = self.Qprime[:,:,:,:,:,self.QoI.PEcol]**2 # eV**2
+                self.Qprime[:,:,:,:,:,qc] = deepcopy(self.Qprime[:,:,:,:,:,self.QoI.PEcol])**2 # eV**2
             elif q == 'HeatCapacityPress':
                 self.Qprime[:,:,:,:,:,qc] = (self.Qprime[:,:,:,:,:,self.QoI.PEcol] + 
                                              self.Qprime[:,:,:,:,:,self.QoI.Pcol]*self.Qprime[:,:,:,:,:,self.QoI.Vcol])**2
@@ -548,13 +508,16 @@ class FuncDer_perturb_coord(FuncDer_perturbative):
                 self.Qprime[:,:,:,:,:,qc] = self.Qprime[:,:,:,:,:,self.QoI.Vcol]**2
             # Volume: pass
 
+        #for qc, q in enumerate(self.QoI.Q_names):
+        #    self.Qprime[:,:,:,:,:,qc] = self.Qprime[:,:,:,:,:,qc]*self.QoI.conversions[qc]
+
 
     def prepare_FD(self):
         self.get_perturbs()
         print("Calculated perturbations")
 
         self.calc_Qprime()
-        print("Calculated perturbation Hamiltonian contribution")
+        print("Calculated perturbation Hamiltonian contributions")
         self.exp_weighting()
         print("Calculated exponential weights")
 

FunUQ/funuq.py
@@ -7,17 +7,13 @@ from random import random; from glob import glob
 from matplotlib import pyplot as plt
 from scipy import integrate
 
-# import local functions
-#from parsetools import *
-#from potential import *
-#from utils import FUQerror
 
 
 class FunUQ(object): 
     '''
-    Class for FUNctional Uncertainty Quantification
+    Class for Functional Uncertainty Quantification
     '''
-    def __init__(self, pot_main, pot_correct, Q_names, Qavg, Qavg_correct, FD=None, R=None):
+    def __init__(self, pot_main, pot_correct, Q_names, Qavg, Qavg_correct, FD=None, R=None, Q_units=None):
         '''
         Defaults for base class; to be modified by user through dictionary as needed
         '''
@@ -34,7 +30,8 @@ class FunUQ(object):
         self.Q_names = Q_names
         self.Qavg = Qavg
         self.Qavg_correct = Qavg_correct
-
+        self.Q_units = Q_units
+        
 
     def read_FD(self, resultsdir, FDnames):
         for qc, q in enumerate(FDnames):
@@ -58,35 +55,63 @@ class FunUQ(object):
 
         self.discrep = self.main_PE - self.correct_PE
 
-        print("Calculated discrepancy")
+        print("Calculated discrepancy\n")
 
 
     def correct(self):
-        #self.funcerr = self.funcder*self.discrep
         self.funcerr = np.zeros(np.shape(self.funcder))
         self.Qcorrection = np.zeros(len(self.Q_names))
         self.Qcorrected = np.zeros(len(self.Q_names))
 
-        for qc in range(len(self.Q_names)):
+        print("Calculated correction terms")
+
+        for qc, q in enumerate(self.Q_names):
             self.funcerr[:,qc] = self.funcder[:,qc]*self.discrep
 
             self.Qcorrection[qc] = integrate.trapz(self.funcerr[:,qc], x=self.rlist)
             self.Qcorrected[qc] = self.Qavg[qc] + self.Qcorrection[qc]
             
-            print(self.Qcorrection[qc], (self.Qavg[qc] - self.Qavg_correct[qc]), self.Qavg[qc], self.Qavg_correct[qc])
-        print("Calculated correction terms")
-
+            print(q)
+            print('FunUQ\t\t Direct\t\t Main\t\t Correction')
+            print('correction\t simulation\t QoI\t\t QoI')
+            print('{0:.3e}\t {1:.3e}\t {2:.3e}\t {3:.3e}\n'.format(self.Qcorrection[qc], (self.Qavg[qc] - self.Qavg_correct[qc]), self.Qavg[qc], self.Qavg_correct[qc]))
 
 
     def plot_discrep(self):
         plt.plot(self.rlist, self.discrep, c='navy')
         plt.scatter(self.rlist, self.discrep, c='navy')
+        plt.ylabel("Functional discrepancy (Potential Energy)")
+        plt.xlabel("Position ($\AA$)")
         plt.show()
 
 
     def plot_funcerr(self, qc):
-        plt.plot(self.rlist, self.funcerr[:,qc], c='navy')
-        plt.scatter(self.rlist, self.funcerr[:,qc], c='navy')
+        fig1, ax1 = plt.subplots(1,1)
+        ax1.plot(self.rlist, self.funcerr[:,qc], c='navy', linewidth=4)
+        #ax1.scatter(self.rlist, self.funcerr[:,qc], c='navy')
+        ax1.fill_between(self.rlist, 0, self.funcerr[:,qc], color='lightblue')
+        ax1.set_ylabel("Functional error {} ({})".format(self.Q_names[qc], self.Q_units[qc]))
+        ax1.set_xlabel("Position ($\AA$)")
+        plt.show()
+
+
+    def plot_correction(self, qc):
+        fig1, ax1 = plt.subplots(1,1)
+        ind = np.arange(2)
+        bar = ax1.bar(ind,
+                      [self.Qcorrection[qc], self.Qavg[qc] - self.Qavg_correct[qc]], 
+                      color='grey', linewidth=4)
+        bar[0].set_color('navy')
+        ax1.set_ylabel("Correction {} ({})".format(self.Q_names[qc], self.Q_units[qc]))
+        ax1.set_xticks(ind)
+        ax1.set_xticklabels(('FunUQ correction', 'Direct Simulation'))
+
+        if self.Qavg[qc] - self.Qavg_correct[qc] < 1e-10:
+            if self.Qcorrection[qc] > 0:
+                ax1.set_ylim([0, self.Qcorrection[qc]*1e5])
+            else: 
+                ax1.set_ylim([self.Qcorrection[qc]*1e5, 0])
+
         plt.show()
 
 

FunUQ/potential.py
@@ -1,7 +1,8 @@
 # FunUQ v0.4, 2018; Sam Reeve; Strachan Research Group
 # https://github.rcac.purdue.edu/StrachanGroup
 
-import sys, os, numpy as np
+import sys, os, numpy as np, shutil, subprocess
+from matplotlib import pyplot as plt
 
 
 class Potential(object):
@@ -9,128 +10,171 @@ class Potential(object):
     Simple potential class
     Supports creation of pair potential through LAMMPS pair_style table
     TODO: Allow direct LAMMPS styles
+          Bool for table or LAMMPS, using either "create" or "write"
+          Each should use copy and accept all kwargs currently in "create_table"
     '''
-    def __init__(self, pot, paramfile=None, paramdir=None,
-                 potfile=None, potdir='', create=True):
-        self.potname = pot
-        if paramfile == None:
-            self.paramfile = '{}.params'.format(self.potname)
+    def __init__(self, pot, create=True, paramfile=None, paramdir=None,
+                 potfile=None, potdir='', 
+                 N=8000, rlo=0.001, rhi=8.0, cut=6.,
+                 r0=1.0, c0=0.0, a0=0.0):
 
-        if paramdir != None:
+        self.potname = pot
+
+        # Here, paramfile is only the coeff for the table creation function below
+        if paramfile == None and create:
+            self.paramfile = '{}.params'.format(self.potname)
+        else: 
+            self.paramfile = paramfile
+
+        if paramdir != None and create:
             self.parampath = os.path.join(paramdir, self.paramfile)
         else:
             self.parampath = self.paramfile
 
-        if potfile == None:
+        if potfile == None and create:
             self.potfile = '{}.table'.format(self.potname)
-            self.potpath = os.path.join(potdir, self.potfile)
+        else: 
+            self.potfile = potfile
+        self.potpath = os.path.join(paramdir, self.potfile)
 
-    def create(self, potdir):
+        if create:
+            self.create_table(N, rlo, rhi, cut) #self.potname, self.parampath) #, savedir=potdir)
+        else:
+            #subprocess.call(['lmp', '-i', self.parampath]) #TODO REMOVE
+            R, PE, style, coeff = read_file(self.potpath)
+            self.R = R
+            self.PE = PE
+            self.pairstyle = style
+            self.paircoeff = coeff
+
+
+    # Need to copy later
+    def copy(self, initdir, potdir):
         self.potdir = potdir
-        R, PE, style, coeff = create_table(self.potname, self.parampath, savedir=potdir)
+        potfile = os.path.join(initdir, self.potname+'.table')
+        shutil.copy(potfile, potdir)
+        R, PE, style, coeff = read_file(potfile)
         self.R = R
         self.PE = PE
         self.pairstyle = style
         self.paircoeff = coeff
 
 
+    def plot(self):
+        plt.scatter(self.R, self.PE, c='navy')
+        plt.ylim([-0.2, 0.2])
+        plt.xlim([2, 12])
+        plt.show()
 
-class TableException(Exception):
-    pass
 
 
-
-def create_table(style, potparams, smooth='4th', N=8000, 
-                 rlo=0.001, rhi=8.0, cut=6.0,
-                 r0=1.0, c0=0.0, a0=0.0, 
-                 smooth_w=1.5, 
-                 outname='', savedir=''):
-    '''
-    Creates LAMMPS "pair_style table" file
-
-    Current options:
+    def create_table(self, N, rlo, rhi, cut, 
+                     potname=None, smooth='4th', 
+                     r0=1.0, c0=0.0, a0=0.0, 
+                     smooth_w=1.5, 
+                     outname=None, savedir=None):
+        '''
+        Creates LAMMPS "pair_style table" file
+        
+        Current options:
         style: LJ, Morse
         smooth: 4th order multiplicative smoothing function
         
         N: Number of points
         minR/maxR: Limits of distance tabulatec
         r0/a0/c0: Gaussian perturbations for FunUQ
-
-    To be used with LAMMPS "metal" (eV and Angstrom)
-    '''
-
-    if not outname:
-        outname = '{}.table'.format(style)
+        
+        To be used with LAMMPS "metal" (eV and Angstrom)
+        '''
+        
+        if potname == None: potname = self.potname
+        if outname == None: outname = self.potfile
+        if savedir == None: savedir = self.potpath
+        #if not outname:
+        #    outname = '{}.table'.format(style)
     
-    key = "{}_1".format(style)
-    head = ("# pair_style table for LAMMPS; Created by {};"
-            " Sam Reeve; potential: {}; smoothing: {} \n\n"
-            "{}\n"
-            "N {}\n\n").format(sys.argv[0], style, smooth, key, N)
-    pairstyle = "pair_style table linear {}".format(N)
-    paircoeff = "pair_coeff * * ../{} {} {}".format(outname, key, cut)
+        key = "{}_1".format(potname)
+        head = ("# pair_style table for LAMMPS; Created by {};"
+                " Sam Reeve; potential: {}; smoothing: {} \n\n"
+                "{}\n"
+                "N {}\n\n").format(sys.argv[0], potname, smooth, key, N)
+        self.pairstyle = "pair_style table linear {}".format(N)
+        self.paircoeff = "pair_coeff * * ../{} {} {}".format(outname, key, cut)
 
-    R = np.linspace(rlo, rhi, N)
-    cut_id = np.where(R > cut)[0][0]
+        R = np.linspace(rlo, rhi, N)
+        cut_id = np.where(R > cut)[0][0]
 
 
-    if style != 'gauss':
-        params = read_params(potparams)
+        if potname != 'gauss':
+            params = read_params(self.parampath)
 
-    if style == 'lj':
-        epsilon, sigma = params
+        if potname == 'lj':
+            epsilon, sigma = params
 
-        F = 24*epsilon/sigma*(2*(sigma/R)**13 - (sigma/R)**7)
-        PE = 4*epsilon*((sigma/R)**12 - (sigma/R)**6)
-    elif style == 'morse':
-        D0, alpha, r0_M = params
+            F = 24*epsilon/sigma*(2*(sigma/R)**13 - (sigma/R)**7)
+            PE = 4*epsilon*((sigma/R)**12 - (sigma/R)**6)
+        elif potname == 'morse':
+            D0, alpha, r0_M = params
 
-        F = 2*alpha*D0*(np.exp(-2*alpha*(R - r0_M)) - np.exp(-alpha*(R - r0_M)))
-        PE = D0*(np.exp(-2*alpha*(R - r0_M)) - 2*np.exp(-alpha*(R - r0_M)))
+            F = 2*alpha*D0*(np.exp(-2*alpha*(R - r0_M)) - np.exp(-alpha*(R - r0_M)))
+            PE = D0*(np.exp(-2*alpha*(R - r0_M)) - 2*np.exp(-alpha*(R - r0_M)))
 
-    elif style == 'exp6':
-        epsilon, alpha, r0_E6 = params
+        elif potname == 'exp6':
+            epsilon, alpha, r0_E6 = params
         
-        F = 6*epsilon/(1.-6./alpha)/r0_E6*(np.exp(alpha*(1 - R/r0_E6)) - (r0_E6/R)**7)
-        PE = epsilon/(1.-6./alpha)*(6./alpha*np.exp(alpha*(1 - R/r0_E6)) - (r0_E6/R)**6)
+            F = 6*epsilon/(1.-6./alpha)/r0_E6*(np.exp(alpha*(1 - R/r0_E6)) - (r0_E6/R)**7)
+            PE = epsilon/(1.-6./alpha)*(6./alpha*np.exp(alpha*(1 - R/r0_E6)) - (r0_E6/R)**6)
 
-    elif style == 'fake_exp6':
-        epsilon, alpha, r0_E6 = params
+        elif potname == 'fake_exp6':
+            epsilon, alpha, r0_E6 = params
         
-        F = 2*alpha*epsilon*(np.exp(-2*alpha*(R - r0_E6)) - (r0_E6/R)**7)
-        PE = epsilon*(np.exp(-2*alpha*(R - r0_E6)) - 2*(r0_E6/R)**6)
+            F = 2*alpha*epsilon*(np.exp(-2*alpha*(R - r0_E6)) - (r0_E6/R)**7)
+            PE = epsilon*(np.exp(-2*alpha*(R - r0_E6)) - 2*(r0_E6/R)**6)
         
-    elif style == 'gauss':
-        F = np.zeros([N])
-        PE = np.zeros([N])
+        elif potname == 'sin_test':
+            epsilon, sigma = params
 
-    else:
-        raise TableException("Style not supported.\n\n")
-        exit()
+            F = 24*epsilon/sigma*(2*(sigma/R)**13 - (sigma/R)**7) -0.0782*np.cos(0.17*(24.2+R))
+            PE = 4*epsilon*((sigma/R)**12 - (sigma/R)**6) +0.44+0.49*np.sin(0.19*(R+24.))
+
+        elif potname == 'gauss':
+            F = np.zeros([N])
+            PE = np.zeros([N])
+            
+        else:
+            raise TableException("Style not supported.\n\n")
+            exit()
 
 
-    if smooth == '4th':
-        smooth = ((R - cut)/smooth_w)**4/(1 + ((R - cut)/smooth_w)**4)
-        smoothdiff = (4*smooth_w**4*(cut-R)**3)/(smooth_w**4+(cut-R)**4)**2
+        if smooth == '4th':
+            smooth = ((R - cut)/smooth_w)**4/(1 + ((R - cut)/smooth_w)**4)
+            smoothdiff = (4*smooth_w**4*(cut-R)**3)/(smooth_w**4+(cut-R)**4)**2
 
-        F = PE*smoothdiff + F*smooth
-        PE = PE*smooth
+            F = PE*smoothdiff + F*smooth
+            PE = PE*smooth
 
-        F[cut_id:] = np.zeros(np.shape(R[cut_id:])) # shift
-        PE[cut_id:] = np.zeros(np.shape(R[cut_id:])) # shift
+            F[cut_id:] = np.zeros(np.shape(R[cut_id:])) # shift
+            PE[cut_id:] = np.zeros(np.shape(R[cut_id:])) # shift
   
 
-    if abs(a0) > 0.0:
-        # add gauss after smoothing
-        gaussdiff = (R-r0)/(np.sqrt(2*np.pi)*c0**3) * a0*np.exp(-(R - r0)**2/(2*c0**2)) ### sign
-        F += gaussdiff
-        gauss = 1/(np.sqrt(2*np.pi)*c0) * a0*np.exp(-(R - r0)**2/(2*c0**2))
-        PE += gauss
+        if abs(a0) > 0.0:
+            # add gauss after smoothing
+            gaussdiff = (R-r0)/(np.sqrt(2*np.pi)*c0**3) * a0*np.exp(-(R - r0)**2/(2*c0**2)) ### sign
+            F += gaussdiff
+            gauss = 1/(np.sqrt(2*np.pi)*c0) * a0*np.exp(-(R - r0)**2/(2*c0**2))
+            PE += gauss
 
 
-    write_file(os.path.join(savedir, outname), head, N, R, PE, F)
+        write_file(savedir, head, N, R, PE, F)
+        self.PE = PE
+        self.R = R
 
-    return R, PE, pairstyle, paircoeff
+        return
+
+
+
+class TableException(Exception):
+    pass
 
 
 
@@ -140,6 +184,16 @@ def write_file(fname, txt, N, R, PE, F):
 
     with open(fname, 'w') as f:
         f.write(txt)
+
+
+def read_file(fname): 
+    data = np.loadtxt(fname, skiprows=4)
+    R = data[:,1]
+    PE = data[:,2]
+    style = ''
+    coeff = ''
+    return R, PE, style, coeff
+
                    
 def read_params(fname):
     '''

FunUQ/qoi.py
@@ -5,9 +5,10 @@
 import sys, os, subprocess, shutil, numpy as np
 from random import random; from glob import glob
 from matplotlib import pyplot as plt
+from copy import deepcopy
 
 # import local functions
-from .utils import is_thermo, is_fluct, copy_files, read_template, replace_template, submit_lammps
+from .utils import is_thermo, is_fluct, copy_files, read_template, replace_template, submit_lammps, FUQerror
 from .parsetools import read_thermo, find_columns
 
 
@@ -16,7 +17,7 @@ class QuantitiesOfInterest(object):
     '''
     Class for running LAMMPS and extracting thermodynamic results
     '''
-    def __init__(self, Qlist, potential, initdir, maindir, purpose, input_dict=None):
+    def __init__(self, Qlist, potential, initdir, maindir, purpose, unittype, input_dict=None):
         '''
         Defaults for base class; to be modified by user through dictionary as needed
         '''
@@ -26,7 +27,7 @@ class QuantitiesOfInterest(object):
 
         # Quantity of interest
         self.Q_names = list(Qlist)
-        requiredQ = ['PotEng'] #, 'Press', 'Volume']
+        requiredQ = ['PotEng', 'Press', 'Volume']
         for rQ in requiredQ:
             if rQ not in self.Q_names:
                 self.Q_names += [rQ]
@@ -47,6 +48,7 @@ class QuantitiesOfInterest(object):
         self.pot = potential 
 
         self.units = []
+        self.unittype = unittype
         #self.get_units()
 
         # Files and directories
@@ -71,6 +73,7 @@ class QuantitiesOfInterest(object):
         self.copy_folder = 'copy_'
         self.copy_start = 0
 
+        self.create_pot = True
 
         # User overrides defaults
         if input_dict != None:
@@ -93,10 +96,12 @@ class QuantitiesOfInterest(object):
                 os.mkdir(d)
             except: pass
 
-        # Create potential table
-        #if self.createpot:
-        self.pot.create(self.rundir)
-
+        # COPY potential table
+        #if self.create_pot:
+        #    self.pot.create(self.rundir)
+        #else:
+        #    self.pot.copy(self.initdir, self.rundir)
+        self.pot.copy(self.initdir, self.rundir)
 
 
     def run_lammps(self):
@@ -119,9 +124,14 @@ class QuantitiesOfInterest(object):
         if not self.overwrite:
             existing = glob(os.path.join(rundir, 'copy_*'))
             for x in existing:
-                nextcopy = float(x.split(self.copy_folder)[-1])
+                nextcopy = int(x.split(self.copy_folder)[-1])
                 if nextcopy > maxcopy:
                     maxcopy = nextcopy 
+            if maxcopy > 0:
+                maxcopy += 1
+        elif self.copy_start > 0:
+            maxcopy = self.copy_start
+        # TODO can't start at fixed number and not overwrite
 
         for copy in range(maxcopy, maxcopy+self.Ncopies):
             cdir = os.path.join(rundir, self.copy_folder+str(copy))
@@ -155,11 +165,9 @@ class QuantitiesOfInterest(object):
 
                 # Find names of thermo/fluctuation properties
                 Q_thermonames = []
-                #Q_fluctnames = []
                 for q,qthermo in zip(self.Q_names, self.Q_thermo):
                     if qthermo:
                         Q_thermonames += [q]
-                    #else: self.Q_fluctnames += [q]
 
                 # Get columns for thermo properties
                 Q_cols = find_columns(cols, Q_thermonames) #self.Q_names)
@@ -174,7 +182,6 @@ class QuantitiesOfInterest(object):
                 self.beta = 1./8.617e-5/self.T
 
 
-            #for qc, (q,thermoq) in enumerate(zip(self.Q_cols, self.Q_thermo)):
             for qc, (q,qn) in enumerate(zip(self.Q_cols, self.Q_names)):
                 if qn == 'PotEng':
                     self.PEcol = qc
@@ -188,7 +195,7 @@ class QuantitiesOfInterest(object):
                     self.Q[:, copy0, 0,0,0, qc] = thermo[:,q]
                     #self.Q[:,copy0,qc] = thermo[:,q]
                 elif qn == 'HeatCapacityVol':
-                    self.Q[:, copy0, 0,0,0, qc] = thermo[:,self.Q_cols[self.PEcol]]**2
+                    self.Q[:, copy0, 0,0,0, qc] = deepcopy(thermo[:,self.Q_cols[self.PEcol]])**2
                 elif qn == 'HeatCapacityPress':
                     self.Q[:, copy0, 0,0,0, qc] = (thermo[:,self.Q_cols[self.PEcol]]
                                                    + (thermo[:,self.Q_cols[self.Pcol]]*
@@ -203,12 +210,13 @@ class QuantitiesOfInterest(object):
 
                 # TODO: if there are issues, fill with NaN to ignore
                 # Works okay while jobs running IF the first copy stays ahead
-        
+
 
         self.Qavg = np.mean(self.Q, axis=(0,1,2,3,4))
         self.Qstd = np.std(self.Q, axis=(0,1,2,3,4))
+        print (self.Qavg)
 
-        self.get_conversions()
+
         for qc, q in enumerate(self.Q_names):
 
             if q == 'HeatCapacityVol':
@@ -221,6 +229,7 @@ class QuantitiesOfInterest(object):
                 self.Qavg[qc] = fluctuation(self.beta/self.T/self.V, self.Qavg[qc], self.Qavg[self.PEcol] + self.Qavg[self.Pcol]*self.Qavg[self.Vcol])
 
         # ONLY CONVERT after all fluctuations calculated
+        self.get_conversions()
         for qc, q in enumerate(self.Q_names):
             self.Qavg[qc] = self.Qavg[qc]*self.conversions[qc]
 
@@ -229,10 +238,16 @@ class QuantitiesOfInterest(object):
         self.conversions = [1.]*len(self.Q_names)
 
         for qc, q in enumerate(self.Q_names):
-            if q == 'PotEng':
-                self.conversions[qc] = 1./self.Natoms
+            if q == 'PotEng' or q == 'E_vdwl':
+                if self.unittype == 'metal':
+                    self.conversions[qc] = 1./self.Natoms
+                elif self.unittype == 'real':
+                    self.conversions[qc] = 1. #0.0433644/self.Natoms
             elif q == 'Press':
-                self.conversions[qc] = 0.0001
+                if self.unittype == 'metal':
+                    self.conversions[qc] = 0.0001
+                elif self.unittype == 'real':
+                    self.conversions[qc] = 0.000101325
             elif q == 'Volume':
                 self.conversions[qc] = 0.001
             elif 'HeatCapacity' in q:
@@ -242,37 +257,8 @@ class QuantitiesOfInterest(object):
             elif q == 'ThermalExpansion':
                 self.conversions[qc] = 1.
 
-
-    def write_something(self, qc):
-        txt = ("FunUQ v0.4, 2018; Sam Reeve\n"
-               "Functional derivative for {} in {} with {} at {}K with {} atoms; "
-               "Description: {}\n"
-               .format(self.Q_names[qc], self.units[qc], self.pot.potname, self.T, self.Natoms, self.description))
-        for r, fd in zip(self.rlist, self.funcder[:,qc]):
-            txt += "{} {}\n".format(r, fd)
-
-        name = '{}_{}'.format(self.Q_names[qc], self.FDname)
-        with open(os.path.join(self.resultsdir, name), 'w') as f:
-            f.write(txt)
-
-
-    def plot_something(self, qc):
-        plt.figure()
-        plt.plot(self.rlist, self.funcder[:,qc], color='navy')
-        plt.scatter(self.rlist, self.funcder[:,qc], color='navy')
-        plt.xlabel("Position ($\AA$)", fontsize='large')
-        plt.xlim([self.rmin, self.rmax-0.2])
-        x_range = np.where((self.rlist > self.rmin) & (self.rlist < self.rmax))
-        ymin = np.min(self.funcder[x_range,qc])
-        ymin += -ymin*0.1
-        ymax = np.max(self.funcder[x_range,qc])
-        ymax += ymax*0.1
-        plt.ylim([ymin, ymax])
-        plt.ylabel("Functional Derivative ({}/$\AA$)".format(self.units[qc]), fontsize='large')
-        plt.show()
-
     
-def fluctuation(pre, avg_square, avg):
+def fluctuation(pre, avg_ofthe_square, avg):
 
-    return pre*(avg_square - avg**2)
+    return pre*(avg_ofthe_square - avg**2)
 

examples/nvt.py
@@ -11,32 +11,31 @@ run = False
 run_verify = False
 run_perturb = False
 run_bruteforce = False
-################################################################### Scale QoI values?
 
-# Main inputs
-startdir = 'morse_npt_fluct/'
-rundir = '/scratch/halstead/s/sreeve/FunUQ/active/npt_morse_liquid/'
+# Define directories
+startdir = 'morse_fluct/'
+rundir = '/scratch/halstead/s/sreeve/FunUQ/active/morse_liquid/'
 
-# Potentials
+# Define/create potentials
 Pot_main = Potential('morse', paramdir=startdir, create=True, N=8000, rhi=8.0, cut=6.)
 Pot_correct = Potential('exp6', paramdir=startdir, create=True, N=8000, rhi=8.0, cut=6.)
 
-# QoI
-QoI_list = ['PotEng', 'Press', 'Volume'] #, 'HeatCapacityPress', 'Compressibility', 'ThermalExpansion']
-
+# Define QoI
+QoI_list = ['PotEng', 'Press', 'Volume', 'HeatCapacityVol'] #, 'Compressibility', 'ThermalExpansion']
+Nqoi = len(QoI_list)
 QoI_dict = {'description': 'Test liquid',
             'Ncopies': 2,
-            'units': ['eV/atom', 'GPa', 'nm**3'], #'eV/atom/K', '1/GPa', '1/K'],
+            'units': ['eV/atom', 'GPa', 'nm**3', 'eV/atom/K'] #, '1/GPa', '1/K'],
             #'overwrite': True
 }
 
 QoI = QuantitiesOfInterest(QoI_list, Pot_main, 
-                           startdir, rundir, 'main',
+                           startdir, rundir, 'main', 'metal',
                            input_dict=QoI_dict)
 QoI_correct = QuantitiesOfInterest(QoI_list, Pot_correct,
-                                   startdir, rundir, 'exp6',
+                                   startdir, rundir, 'exp6', 'metal',
                                    input_dict=QoI_dict)
-
+# Run simulations (if needed)
 if run:
     QoI.run_lammps()
 if run_verify:
@@ -47,53 +46,53 @@ if run or run_verify:
 QoI.extract_lammps()
 QoI_correct.extract_lammps()
 
-print(QoI.Qavg, QoI_correct.Qavg)
+# Define functional derivative inputs
+FD_dict = {'alist': [-1e-6, -2e-6, 1e-6, 2e-6], #coord
+           #'alist': [-1e-12, -2e-12, 1e-12, 2e-12],#allatom
 
-FD_dict = {'alist': [-1e-10, -2e-10, 1e-10, 2e-10], #coord
-           #'alist': [-2e-12, -1e-12, 1e-12, 2e-12], #allatom
-           #'alist': [-0.02, -0.01, 0.01, 0.02], #brute                                                                    
-#'rN': 60
-           #'rlist': np.linspace(0.0998333333333333, 6.09983333333333334, 30)
-           
            # Bruteforce
-#'rmin': 1.5,
-#'rlist': np.linspace(1.5, 6., 30),
+           #'alist': [-0.02, -0.01, 0.01, 0.02],
+           #'rmin': 1.5,
+           #'rlist': np.linspace(1.5, 6., 30),
            }
 #FuncDer = FuncDer_bruteforce(QoI, Pot_main,
 #                             input_dict=FD_dict)
-#FuncDer = FuncDer_perturb_allatom(QoI, Pot_main, ensemble='npt',
+#FuncDer = FuncDer_perturb_allatom(QoI, Pot_main,
 #                                   input_dict=FD_dict)
-FuncDer = FuncDer_perturb_coord(QoI, Pot_main, ensemble='npt',
+FuncDer = FuncDer_perturb_coord(QoI, Pot_main, ensemble='nvt',
                                 input_dict=FD_dict)
 
 
-# Calculate functional derivative
+# Run simulations for functional derivative (if needed)
 if run_bruteforce:
     FuncDer.run_lammps()
     exit() 
-
-if run_perturb and FuncDer.method == 'perturb_allatom':
+if run_perturb and FuncDer.method == 'perturbative_allatom':
     FuncDer.rerun_gauss()
     exit()
 
+# Calculate functional derivative
 FuncDer.prepare_FD()
 FuncDer.calc_FD()
-for x in range(len(QoI_list)):
+for x in range(Nqoi-1,Nqoi):
     FuncDer.write_FD(x)
-    #FuncDer.plot_FD(x)
+    FuncDer.plot_FD(x)
+    
 
+# Prepare correction
+Correct = FunUQ(Pot_main, Pot_correct, QoI.Q_names, QoI.Qavg, QoI_correct.Qavg, Q_units=QoI.units, FD=FuncDer.funcder, R=FuncDer.rlist)
 
-Correct = FunUQ(Pot_main, Pot_correct, QoI.Q_names, QoI.Qavg, QoI_correct.Qavg, FD=FuncDer.funcder, R=FuncDer.rlist)
+# Read FD instead of recomputing
 #Correct = FunUQ(Pot_main, Pot_correct, QoI.Q_names, QoI.Qavg, QoI_correct.Qavg)
-
-# Get corrections for properties
 #Correct.read_FD(QoI.resultsdir, FuncDer.FDnames)
 
+# Calculate functional discrepancy
 Correct.discrepancy()
+#Correct.plot_discrep()
 
 # Get corrections for properties
 Correct.correct()
-
-#for x in range(len(QoI_list)):
+#for x in range(Nqoi):
 #    Correct.plot_funcerr(x)
+#    Correct.plot_correction(x)
 